e7838785290e7c23ab75610e239643f978360f09
h4zm1 <adolf.hazmi@yahoo.com>
2025-12-02 00:44:38 +0100

    Fix: scss file get bigger on deploy

--- src/app/core/underscreencard/underscreencard.component.ts
+++ src/app/core/underscreencard/underscreencard.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { NgStyle} from "@angular/common";
+
+interface MeteorStyle {
+  id: number;
+  style: {
+    '--top': string;
+    '--left': string;
+    '--duration': string;
+  };
+}
 
 @Component({
     selector: 'app-underscreencard',
-    imports: [],
+  imports: [
+    NgStyle
+  ],
     templateUrl: './underscreencard.component.html',
     styleUrl: './underscreencard.component.scss'
 })
-export class UnderscreencardComponent {
+export class UnderscreencardComponent implements OnInit{
+  meteors: MeteorStyle[] = [];
+  meteorCount = 15;
 
+  ngOnInit() {
+    this.generateMeteorStyles();
+  }
+
+  generateMeteorStyles() {
+    for (let i = 0; i < this.meteorCount; i++) {
+      const v = Math.random() * 100 - 35; // left (-35% to 65%)
+      const h = Math.random() * 350 * -1; // top (0 to -350px)
+      const d = Math.random() * 5 + 2; // duration (2s to 7s)
+
+      this.meteors.push({
+        id: i,
+        style: {
+          '--top': `${h}px`,
+          '--left': `${v}%`,
+          '--duration': `${d}s`
+        }
+      });
+    }
+  }
 }
