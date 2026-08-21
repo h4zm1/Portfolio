2ad2ac2e4afe08d2bf903b0d6d499a19692d5f36
h4zm1 <mosbah.hazmi@gmail.com>
2026-08-21 22:46:21 +0100

    Chore: prevent server from making http calls for icon registration

--- src/app/shared/icon.service.ts
+++ src/app/shared/icon.service.ts
@@ -6,31 +6,29 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { Observable, map, forkJoin, catchError, of, tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class IconService {
   private http = inject(HttpClient);
   private iconRegistry = inject(MatIconRegistry);
   private sanitizer = inject(DomSanitizer);
-
+  private platformId = inject(PLATFORM_ID);
 
   registerIcons(): Observable<void> {
-    return this.http.get<Record<string, string>>('icons.config.json')
-      .pipe(
-        tap(config => {
-          Object.entries(config).forEach(([name, path]) => {
-            this.iconRegistry.addSvgIcon(
-              name,
-              this.sanitizer.bypassSecurityTrustResourceUrl(path)
-            );
-          });
-        }),
-        map(() => void 0) // Convert to Observable<void>
-      );
+    // skip icon registeration on server
+    if (!isPlatformBrowser(this.platformId)) {
+      return of(void 0);
+    }
+    return this.http.get<Record<string, string>>('icons.config.json').pipe(
+      tap((config) => {
+        Object.entries(config).forEach(([name, path]) => {
+          this.iconRegistry.addSvgIcon(
+            name,
+            this.sanitizer.bypassSecurityTrustResourceUrl(path),
+          );
+        });
+      }),
+      map(() => void 0), // Convert to Observable<void>
+    );
   }
-
-
 }
-
-
-
